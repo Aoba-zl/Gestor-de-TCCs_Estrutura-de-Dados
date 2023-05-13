3b007e471842bfadc5f74680555c8bb20be8dde2
Matheus <matheussss6613@gmail.com>
2023-05-13 12:07:57 -0300

    test

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,8 +2,10 @@ package view;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("aaaa");
 
 	}
 
